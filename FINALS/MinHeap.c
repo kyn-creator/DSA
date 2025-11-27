58abfe503b8f4e82054ac4efe9e4a5a607ba09fc
Kyn Honoridez <gwapomulang5@gmail.com>
2025-11-28 00:29:33 +0800

    updates

--- FINALS/MinHeap.c
+++ FINALS/MinHeap.c
@@ -21,13 +21,16 @@ int main(){
 	BinaryHeap minHeap;
 	minHeap.count = 0;
 	
+	insert(&minHeap,10);
+	insert(&minHeap,18);
+	insert(&minHeap,14);
 	insert(&minHeap,3);
-	insert(&minHeap,4);
-	insert(&minHeap,5);
-	insert(&minHeap,2);
-	insert(&minHeap,1);
+	insert(&minHeap,12);
+	insert(&minHeap,6);
+	insert(&minHeap,8);
 	
-	Display(minHeap);
+	printf("Before delete: ");	Display(minHeap); printf("\n");
+
 	Delete(&minHeap);
 	
 	Display(minHeap);
@@ -40,54 +43,52 @@ int main(){
 
 
 void insert(BinaryHeap *heap,int num){
-	
 	int indx =0;
 	indx = heap->count;
 	int parent = (indx-1)/2;
 	
-	while(indx > 0 && num < heap->elems[indx]){
+	while(indx > 0 && num < heap->elems[parent]){
 		heap->elems[indx] = heap->elems[parent];
-		indx = parent;	
-		parent = (indx-1)/2;
+		indx = parent;
+		parent = (indx-1)/2;  
 	}
 	
 	heap->elems[indx] = num;
 	heap->count++;
-	
 }
 
 void Delete(BinaryHeap *heap){
-	
 	heap->elems[0] = heap->elems[heap->count-1];
 	heap->count--;
-	int parent=0;
+	
+	int parent =0;
+	
 	while(true){
-		int idx=parent, left = 2*parent+1,right =2*parent+2;
+		int indx = parent;
+		int left = (2*parent)+1;
+		int right = (2*parent)+2;
 		
-		if(left < heap->count && heap->elems[left] < heap->elems[idx]){
-			idx = left;
+		if(left > heap->count && heap->elems[left] < heap->elems[indx] ){
+			indx = left;
 		}
 		
-		if(right < heap->count&&heap->elems[right]<heap->elems[idx]){
-			idx = right;
+		if(right > heap->count && heap->elems[right] < heap->elems[indx]){
+			indx = right;
 		}
+		if(parent == indx)return;
 		
-		if(idx == parent){
-			break;
-		}
-		
-		int temp = heap->elems[idx];
-		heap->elems[idx] = heap->elems[parent];
+		int temp = heap->elems[indx];
+		heap->elems[indx] = heap->elems[parent];
 		heap->elems[parent] = temp;
-		
-		parent = idx;
+		parent = indx;
 	}
 }
+
 
 void Display(BinaryHeap heap){
 	int  i;
 	for(i=0 ;i<heap.count;i++){
-		printf("%d",heap.elems[i]);
+		printf("%d ",heap.elems[i]);
 	}
 	printf("\n");
 }
